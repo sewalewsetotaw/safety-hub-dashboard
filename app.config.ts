f56657f919034ecfb1d006fa6782b2a80cf19e52
sewalew <sewalewsetotaw2023@gmail.com>
2026-04-27 03:01:10 +0300

    add nitro to vite.config

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,9 @@
-// app.config.ts
 import { defineConfig } from '@tanstack/start/config'
+import { vercelAdapter } from '@tanstack/start-adapter-vercel'
 
 export default defineConfig({
   server: {
     preset: 'vercel',
   },
+  adapter: vercelAdapter(), 
 })
